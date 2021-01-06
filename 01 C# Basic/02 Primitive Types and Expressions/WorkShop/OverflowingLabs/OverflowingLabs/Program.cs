1d4521d6e9b3e0aff0e1bd3abc46e45134ebf3cd
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2021-01-06 22:11:23 +0100

    02 Primitive Types and Expressions

---
+++ 01 C# Basic/02 Primitive Types and Expressions/WorkShop/OverflowingLabs/OverflowingLabs/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverflowingLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float number = 1.2f;
+            double number2 = 1.2;
+            decimal money = 1.2m;
+            short length = 0;
+
+
+            byte value = 255;
+            int a = 275;
+            value = (byte)275;
+            Console.WriteLine(value);
+
+
+
+            //try
+            //{
+            //    checked
+            //    {
+            //        byte value = 255;
+            //        value += 2;
+            //        Console.WriteLine(value);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+
+            //    // handler execption
+            //    Console.WriteLine("overflow occurs !!");
+            //}
+
+            //byte b = 1;
+            //int a = b;
+
+            //int a = 256;
+            //byte b = (byte)a;
+
+            string s = "abc";
+            int i = Convert.ToInt32(s);
+            int ii = int.Parse(s);
+
+        }
+    }
+}
--- 01 C# Basic/02 Primitive Types and Expressions/WorkShop/OverflowingLabs/OverflowingLabs/Properties/AssemblyInfo.cs
+++ 01 C# Basic/02 Primitive Types and Expressions/WorkShop/OverflowingLabs/OverflowingLabs/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ApplicationDemo.BusinessLayer")]
+[assembly: AssemblyTitle("OverflowingLabs")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ApplicationDemo.BusinessLayer")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyProduct("OverflowingLabs")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5e791375-00be-4442-972b-91408b147770")]
+[assembly: Guid("2bc24bfc-c003-4d64-8a99-3c8662e688db")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyKeyFile("..\\key.snk")]
