5b3ac63b9d2569a3dfe206b9681a2c75f33fec1f
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2021-01-08 20:08:19 +0100

    ReferenceTypeVsValueTypes|Conditional|Iterations

---
+++ 01 C# Basic/03 Non-Primitive Types/WorkShop/Demo/Demo/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace Demo
+{
+    public class Person
+    {
+        #region Fields
+        public int Age;
+        public decimal salary;
+        public decimal salary2;
+        public decimal salary3;
+        #endregion
+
+        #region Contructors
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region operator
+        public static explicit operator decimal(Person p)
+        {
+            // Logic
+            return p.salary;
+        }
+        public static implicit operator int(Person p)
+        {
+            return p.Age;
+        }
+        #endregion
+    }
+}
+
---
+++ 01 C# Basic/03 Non-Primitive Types/WorkShop/Demo/Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number); // 11
+
+            IncrementResult(number);
+            //Console.WriteLine(result); 
+
+            var person = new Person { Age = 20, salary = 900 };
+
+            MakeOld(person);
+            Console.WriteLine(person.Age); // 30
+
+            //decimal salary = person; // implicit
+            decimal salary = (decimal)person; // explicit
+
+            int age = person;
+        }
+
+        public static void Increment(int value)
+        {
+            value += 10;
+        }
+
+        public static int IncrementResult(int value)
+        {
+            value += 10;
+            return value;
+        }
+        public static void MakeOld(Person obj)
+        {
+            obj.Age += 10;
+        }
+    }
+}
