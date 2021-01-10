83feee3d33ff1803df513d24a35b8f40546181b2
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2021-01-10 18:29:38 +0100

    05 Arrays and Lists|06 Working with Dates|07 Working with Text

---
+++ 01 C# Basic/07 Working with Text/WorkShop/StringLabs/StringLabs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringLabs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string value = null;
+            //Program x;
+            
+            //int number = Convert.ToInt32(value);
+            ////number = null;
+            //var number2 = int.Parse(value);
+            string fullName = "Ahmed Hedfi ";
+            //fullName = fullName.Trim();
+            Console.WriteLine("Trim : '{0}'", fullName.Trim());
+            Console.WriteLine("ToUpper : '{0}'", fullName.Trim().ToUpper());
+            int index = fullName.IndexOf(' ');
+            string firstName = fullName.Substring(0, index);
+            string lastName = fullName.Substring(index + 1);
+            Console.WriteLine("firstName : '{0}'", firstName);
+            Console.WriteLine("lastName : '{0}'", lastName);
+            var replacedString = fullName.Replace("ahmed", "hello");
+            Console.WriteLine(fullName.Replace("Ahmed", "hello"));
+            if (String.IsNullOrEmpty(null))
+                Console.WriteLine("Invalid");
+            string str = "100";
+            decimal ammount = Convert.ToDecimal(str);
+            var formattedAmmount = ammount.ToString("C",new CultureInfo("ja-JP"));
+            Console.WriteLine(formattedAmmount);
+
+        }
+    }
+}
