54d611d46dba931cf055fadba877ffed525cda1c
Ahmed HEDFI <ahmed.hedfi@outlook.fr>
2021-01-15 08:56:39 +0100

    08 Working with Files|09 Debugging Applications

---
+++ 01 C# Basic/08 Working with Files/WorkShop/FileLabs/FileLabs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLabs
+{
+    public class Demo
+    {
+        public void DisplyMsg()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"c:\demo\path.txt";
+
+            var pathForInstanceMethod = @"c:\demo\pathForInstanceMethod.txt";
+            // static method
+           
+            if (File.Exists(path))
+            {
+				File.Copy(path, @"c:\demo\path2.txt", true);
+            }
+
+            var content = File.ReadAllText(path);
+            File.Delete(path);
+
+            // instance methods
+            var fileinfo = new FileInfo(pathForInstanceMethod);
+            fileinfo.CopyTo(@"c:\demo\path2.txt", true);
+
+            if (fileinfo.Exists)
+            {
+                //
+            }
+            fileinfo.Delete();
+            #region Directory
+            Directory.CreateDirectory(@"c:\temp\folderC#Demo");
+            var files = Directory.GetFiles(@"c:\demo" , "*.txt" , SearchOption.AllDirectories);
+            foreach (var filePath in files)
+            {
+                Console.WriteLine(File.ReadAllText(filePath));
+            }
+            #endregion
+            #region Path
+            //var fragments = path.Split('\\');
+            //var fileName = fragments[fragments.Length - 1];
+            //var extensionfragment = fileName.Split('.');
+            //var extension = "." + extensionfragment[1]
+
+            Console.WriteLine("Extension : " + Path.GetExtension(path));
+            Console.WriteLine("File Name : " + Path.GetFileName(path));
+            Console.WriteLine("File Name Without Extension : " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory name : " + Path.GetDirectoryName(path));
+            #endregion
+        }
+    }
+}
---
+++ 01 C# Basic/09 Debugging Applications/WorkShop/DebugLabs/DebugLabs/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugLabs
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallests = GetSmallests(numbers, 4);
+
+//#if DEBUG
+//            Console.WriteLine("Debug version");
+//#endif
+            TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
+            TextWriterTraceListener tr2 = new TextWriterTraceListener(System.IO.File.CreateText("Output.txt"));
+
+            Trace.Listeners.Add(tr2);
+
+            Debug.Listeners.Add(tr1);
+            Debug.Listeners.Add(tr2);
+
+            Debug.WriteLine("Debug: Main method");
+            Debug.Flush();
+            Trace.WriteLine($"Trace- {DateTime.Now:yyyy-mm-dd HH:mm:ss}: Main method");
+            Trace.Flush();
+
+            foreach (var number in smallests)
+                Console.WriteLine(number);
+
+            // ....
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            //Defense Programming
+            if (list == null)
+                throw new ArgumentNullException("list should be not null", nameof(list));
+            if (list.Count < count || count <= 0)
+                throw new ArgumentException("The size of smallest number should between 1 and n elements of the list", nameof(count));
+
+            var smallests = new List<int>();
+            //var copyOfElements = new List<int>();
+            //foreach (var number in list)
+            //{
+            //    copyOfElements.Add(number);
+            //}
+            var copyOfElements = new List<int>(list);
+            while (smallests.Count < count)
+            {
+                var min = GetSmallest(copyOfElements);
+                smallests.Add(min);
+                copyOfElements.Remove(min);
+            }
+
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            //if (list == null)
+            //    throw new ArgumentNullException("list should not be null");
+            //if (list.Count == 0)
+            //    throw new ArgumentException("list should not be empty");
+            // Assume the first number is the smallest
+            var min = list[0];
+            for (var i = 1; i < list.Count; i++)
+            {
+                if (list[i] < min)
+                    min = list[i];
+            }
+            return min;
+        }
+    }
+
+}
